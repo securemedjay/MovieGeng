dceb3ef663b69bbad66d5ba829d8ad2c8650a7ba
Abiodun <securemedjay@gmail.com>
2022-08-06 23:21:24 +0100

    include media folder

--- movie_geng_project/settings.py
+++ movie_geng_project/settings.py
@@ -143,8 +143,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-MEDIA_URL = 'images/'
-MEDIA_ROOT = BASE_DIR / "static/images"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
